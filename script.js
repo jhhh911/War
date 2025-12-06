b7720f473296d4ac325959c1dc9dc8df622bf710
James Heiniger <james.heiniger911@gmail.com>
2025-12-06 14:01:47 +1030

    Code refactor

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ let compCard = "";
 let userCard = "";
 const compScorecard = document.getElementById("comp-score");
 const myScorecard = document.getElementById("my-score");
-let compScore = 0;
-let userScore = 0;
 const drawCards = document.getElementById("draw-cards");
 const cardsRemaining = document.getElementById("cards-remaining");
 const finalResult = document.getElementById("title");
+
+let compScore = 0;
+let userScore = 0;
+
+const cardArray = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "JACK",
+  "QUEEN",
+  "KING",
+  "ACE",
+];
 
 async function handleClick() {
   const res = await fetch(
@@ -23,40 +40,24 @@ async function handleClick() {
 document.getElementById("new-deck").addEventListener("click", handleClick);
 
 async function drawNewCards() {
-  if (deckId) {
-    const res = await fetch(
-      `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
-    );
-    const data = await res.json();
-    document.querySelectorAll(".card-slot").forEach((slot, i) => {
-      slot.innerHTML = `<img src='${data.cards[i].image}' class='card'>`;
-      i === 0
-        ? (compCard = data.cards[i].value)
-        : (userCard = data.cards[i].value);
-    });
-    finalResult.innerHTML = determineWinner(compCard, userCard);
-    showRemainingCards(data);
-  }
+  if (deckId) return;
+  const res = await fetch(
+    `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+  );
+  const data = await res.json();
+  document.querySelectorAll(".card-slot").forEach((slot, i) => {
+    slot.innerHTML = `<img src='${data.cards[i].image}' class='card'>`;
+    i === 0
+      ? (compCard = data.cards[i].value)
+      : (userCard = data.cards[i].value);
+  });
+  finalResult.textContent = determineWinner(compCard, userCard);
+  showRemainingCards(data);
 }
 
 drawCards.addEventListener("click", drawNewCards);
 
 function determineWinner(card1, card2) {
-  const cardArray = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "JACK",
-    "QUEEN",
-    "KING",
-    "ACE",
-  ];
   const compValue = cardArray.findIndex(i => card1 === i);
   const userValue = cardArray.findIndex(i => card2 === i);
   if (compValue > userValue) {
@@ -75,7 +76,7 @@ function determineWinner(card1, card2) {
 }
 
 function showRemainingCards(input) {
-  cardsRemaining.innerHTML = `Cards Remaining: ${input.remaining}`;
+  cardsRemaining.textContent = `Cards Remaining: ${input.remaining}`;
   if (!input.remaining) {
     drawCards.disabled = true;
     if (compScore > userScore) {
@@ -91,8 +92,8 @@ function showRemainingCards(input) {
 function resetScores() {
   compScore = 0;
   userScore = 0;
-  compScorecard.innerHTML = `Computer score: ${compScore}`;
-  myScorecard.innerHTML = `My score: ${userScore}`;
+  compScorecard.textContent = `Computer score: ${compScore}`;
+  myScorecard.textContent = `My score: ${userScore}`;
   document
     .querySelectorAll(".card-slot")
     .forEach(slot => (slot.innerHTML = ""));
